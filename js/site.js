ccdd10e96df04dcbe541f0b57a59ee81467c6f09
fedyafed <fedyafed@mail.ru>
2014-11-28 23:28:11 +0300

    Include Handlebars.

--- js/site.js
+++ js/site.js
@@ -1,3 +1,50 @@
 $(document).ready(function(){
-   console.log(123);
+    //---------------------------------------change page---------------------------------------
+    var goToHomePage = function(){
+        loadHomeData();
+        $("#searchForm").show();
+        $("#homePage").show();
+    };
+
+    $(".home").click(goToHomePage);
+
+    var goToSearchPage = function(){
+        $("#errorMessage").hide();
+        $("#searchForm").hide();
+        $("#homePage").hide();
+    };
+
+    $("#searchButton").click(goToSearchPage);
+    $("#searchLink").click(goToSearchPage);
+
+    $("#errorMessage").click(function(){
+        $("#errorMessage").hide();
+    });
+
+    //------------------------------------handlebars--------------------------------------------
+    var featuredProductsTemplate = Handlebars.compile($("#featuredProducts").html());
+    var viewFeaturedProducts = function(data) {
+        console.log(featuredProductsTemplate(data));
+        $("#products")
+            .html(featuredProductsTemplate(data))
+            .show();
+    };
+
+    //------------------------------------JSON-----------------------------------------------
+    var loadHomeData = function() {
+        $("#errorMessage").hide();
+        $("#products").hide();
+
+        $.getJSON("json/featured-products.json", function (data) {
+            console.log(data);
+            viewFeaturedProducts(data);
+        }).fail(function () {
+            $("#errorMessage")
+                .text("Can not load resources.")
+                .show();
+        });
+    };
+
+    //----------------------------------------Initialization------------------------------
+    goToHomePage();
 });
