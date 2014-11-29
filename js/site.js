48e635b6034e0b6dc593b2ecfbfaeb89ac6e41d0
fedyafed <fedyafed@mail.ru>
2014-11-30 01:25:52 +0300

    Add search page. Include moment.js. Include bootstrap slider. Include bootstrap datetimepicker.

--- js/site.js
+++ js/site.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
         loadHomeData();
         $("#searchForm").show();
         $("#homePage").show();
+        $("#searchPage").hide();
     };
 
     $(".home").click(goToHomePage);
@@ -12,6 +13,7 @@ $(document).ready(function(){
         $("#errorMessage").hide();
         $("#searchForm").hide();
         $("#homePage").hide();
+        $("#searchPage").show();
     };
 
     $("#searchButton").click(goToSearchPage);
@@ -46,5 +48,32 @@ $(document).ready(function(){
     };
 
     //----------------------------------------Initialization------------------------------
-    goToHomePage();
+    $("#priceSlider")
+        .slider({
+            min: 0,
+            max: 10000,
+            step: 1,
+            value: [0, 10000],
+            tooltip: 'hide',
+            handle: 'triangle'
+        })
+        .on('slide', function(event) {
+            $("#priceFrom").val(event.value[0]);
+            $("#priceTo").val(event.value[1]);
+        });
+    $(".slider.slider-horizontal").width("100%");
+    $("#priceFrom, #priceTo").change(function() {
+        $("#priceSlider").slider('setValue', [
+            $("#priceFrom").val(),
+            $("#priceTo").val()
+        ])
+    });
+
+    $('#closeDatePicker').datetimepicker({
+            pickTime: false,
+            defaultDate: new Date()
+    });
+
+
+    goToSearchPage();
 });
